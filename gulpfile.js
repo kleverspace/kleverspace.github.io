a3262e3252e3f65208ae43246408bcfb96405448
Jan Klever <janklever@gmail.com>
2022-06-24 22:31:02 -0300

    Site initial version

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp        = require('gulp');
+const sass        = require('gulp-sass')(require('sass'));
+const cleanCSS    = require('gulp-clean-css');
+const pug         = require('gulp-pug');
+const browserSync = require('browser-sync').create();
+
+// Static Server + watching scss/html files
+function serve() {
+  browserSync.init({
+    server: './dist',
+    browser: 'safari',
+  })
+
+  gulp.watch('app/styles/**/*.scss', style)
+  gulp.watch('app/*.pug', html)
+  gulp.watch('dist/*.html').on('change', browserSync.reload)
+}
+
+// Compile pug into html & auto-inject into browsers
+function html() {
+  return gulp.src('app/*.pug')
+    .pipe(pug({
+      locals: {},
+      pretty: true,
+    }))
+    .pipe(gulp.dest('dist'))
+};
+
+// Compile sass into CSS & auto-inject into browsers
+function style() {
+  return gulp.src('app/styles/*.scss')
+    .pipe(sass())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/assets'))
+    .pipe(browserSync.stream());
+};
+
+exports.html = html
+exports.style = style
+exports.serve = serve
+exports.default = gulp.series(html, style, serve)
